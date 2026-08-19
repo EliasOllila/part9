b7588f320cbf3c8ea5f6799712bd3de5d04edb10
eliasollila <elias.ollila99@gmail.com>
2026-08-19 13:13:08 +0300

    9.12

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import diagnosisRouter from './routes/diagnoses.ts';
+import patientRouter from './routes/patients.ts';
+import cors from 'cors';
 
 const app = express();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
@@ -12,6 +17,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosisRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
